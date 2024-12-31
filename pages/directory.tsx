d85e8572c2ba2ae7f443056b3af620ea4b97c37b
Justin De Burgo <jpdeburgo.jp@gmail.com>
2024-12-31 15:26:11 -0600

    adding directory table

--- pages/directory.tsx
+++ pages/directory.tsx
@@ -1,7 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
+import {
+  Heading,
+  VStack,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 
-const columnDefs = [{ field: "firstName" }, { field: "lastName" }];
+const columnDefs = [
+  { field: "firstName", name: "First Name" },
+  { field: "lastName", name: "Last Name" },
+];
 
 const Directory = () => {
   const [directoryData, setDirectoryData] = useState([]);
@@ -10,12 +26,39 @@ const Directory = () => {
     const fetchDirectory = async () => {
       const directoryResponse = await fetch("/api/directory");
       const directoryData = await directoryResponse.json();
-      setDirectoryData(directoryData);
+      setDirectoryData(directoryData.directory);
     };
     fetchDirectory();
   }, []);
 
-  return <AgGridReact rowData={directoryData} columnDefs={columnDefs} />;
+  console.log({ directoryData });
+
+  return (
+    <VStack>
+      <Heading>Directory</Heading>
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption>Imperial to metric conversion factors</TableCaption>
+          <Thead>
+            <Tr>
+              {columnDefs.map((column) => (
+                <Th key={column.field}>{column.name}</Th>
+              ))}
+            </Tr>
+          </Thead>
+          <Tbody>
+            {directoryData.map((row) => (
+              <Tr key={row.id}>
+                {columnDefs.map((column) => (
+                  <Td key={column.field}>{row[column.field]}</Td>
+                ))}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </VStack>
+  );
 };
 
 export default Directory;
